520ec2344065e9143aa0ed9fb2bc999fe2b85d1c
Robert Ciechowski <croberttm@gmail.com>
2020-07-18 16:58:54 +0200

    Revert "Exercise 11.5"
    This reverts commit b98dd86ff2729204765d38c73b241986ead9c062.

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,44 +1,18 @@
 import React from 'react'; // import biblioteki React
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
-import Card from '../Card/Card.js';
-import Creator from '../Creator/Creator.js';
-import { settings } from '../../data/dataStore';
-import Icon from '../Icon/Icon.js';
 
 class Column extends React.Component {
     static propTypes = {
-        title: PropTypes.string,
-        cards: PropTypes.array,
-        icon: PropTypes.node,
-        addCard: PropTypes.func,
-        id: PropTypes.string,
-    };
-
-    static defaultProps = {
-        icon: settings.defaultColumnIcon,
-    };
+        titleColumn: PropTypes.string,
+    }
 
     render() {
-        const {title, icon, cards, addCard} = this.props;
         return (
             <section className={styles.component}>
-                <h3 className={styles.title}>
-                    <span className={styles.icon}>
-                        <Icon name={icon} />
-                    </span>    
-                    {title}
-                </h3> 
-
-                {cards.map((cardData) => (
-                    <Card key={cardData.id} {...cardData} />
-                ))}
-
-                <div>
-                    <Creator text={settings.cardCreatorText} action={addCard} />
-                </div>
+                <h3 className={styles.title}>{this.props.titleColumn}</h3>
             </section>
-        );
+        )
     }
 }
 
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,63 +3,29 @@ import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
 import Column from '../Column/Column.js';
 import PropTypes from 'prop-types';
-import { settings } from '../../data/dataStore';
-import ReactHtmlParser from 'react-html-parser';
-import Creator from '../Creator/Creator.js';
 
 class List extends React.Component { // definicja klasy List. Klasa List dziedziczy z klasy React.Component
-  state = { // dodanie stanu komponentu za pomocą własciwości
-    columns: this.props.columns || [],
-  }
+    static propTypes = { // definicja typów propsów
+        title: PropTypes.node.isRequired, // definicja typu Propsa (node) z zaznaczeniem że jest on wymagany (ten komponent MUSI otrzymać daną wartość)
+        children: PropTypes.node.isRequired, // definicja typu Propsa (node) z zaznaczeniem że jest on wymagany (ten komponent MUSI otrzymać daną wartość)
+        imgUrl: PropTypes.node,
+    }
 
-  static propTypes = { // definicja typów propsów
-    title: PropTypes.node.isRequired, // definicja typu Propsa (node) z zaznaczeniem że jest on wymagany (ten komponent MUSI otrzymać daną wartość)
-    //children: PropTypes.node.isRequired, // definicja typu Propsa (node) z zaznaczeniem że jest on wymagany (ten komponent MUSI otrzymać daną wartość)
-    image: PropTypes.node,
-    description: PropTypes.node, // deklaracja typów z dataStore
-    columns: PropTypes.array, // deklaracja typów z dataStore
-  }
-
-  static defaultProps = { // ustawienie domyślnych wartości parametrów, kiedy nie zostanie podana żadna zawartośc opisu listy, czyli to jest domyślny opis listy
-    //children: <p>I can do all the things!!!</p>
-    description: settings.defaultListDescription, // nowa domyślna wartość propsów
-  }
-
-  addColumn(title){
-    this.setState(state => (
-      {
-        columns: [
-          ...state.columns,
-          {
-            key: state.columns.lenght ? state.columns[state.columns.lenght-1].key+1 : 0,
-            title,
-            icon: 'list-alt',
-            cards: []
-          }
-        ]
-      }
-    ));
-  }
-
-  render() { // metoda render - od niej zależy to co wyświetli się w przeglądarce
-    const {title, image, description, columns, addColumn} = this.props;
+    static defaultProps = { // ustawienie domyślnych wartości parametrów, kiedy nie zostanie podana żadna zawartośc opisu listy, czyli to jest domyślny opis listy
+      children: <p>I can do all the things!!!</p>
+    }
+    
+    render() { // metoda render - od niej zależy to co wyświetli się w przeglądarce
     return ( // metoda render zwraca obiekt JSX, a konkretniej div
       <section className={styles.component}>
-        <Hero titleText={this.props.title} image={this.props.image} />
+        <Hero titleText={this.props.title} titleImgUrl={this.props.imgUrl} />
         <div className={styles.description}>
-          {ReactHtmlParser(this.props.description)}
+            {this.props.children}
         </div>
         <div className={styles.columns}>
-          {/*<Column titleColumn={'Animals'} />
+          <Column titleColumn={'Animals'} />
           <Column titleColumn={'Plants'} />
-          <Column titleColumn={'Minerals'} />*/}
-          {this.state.columns.map(({key, ...columnProps}) => (
-            <Column key={key} {...columnProps} />
-          ))}
-        </div>
-
-        <div className={styles.creator}>
-          <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)} />
+          <Column titleColumn={'Minerals'} />
         </div>
       </section>
     )
