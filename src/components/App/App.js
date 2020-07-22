0e4b8089459d809d403baef09ece07fb8f99c0c5
Robert Ciechowski <croberttm@gmail.com>
2020-07-22 17:53:36 +0200

    Import and use objects pageContents and listData in App.js component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,16 @@
 import React from 'react'; // import biblioteki React
 import styles from './App.scss';
 import List from '../List/List.js';
+import {pageContents, listData} from '../../data/dataStore';
 
 class App extends React.Component { // definicja klasy App. Klasa App dziedziczy z klasy React.Component
   render() { // metoda render - od niej zależy to co wyświetli się w przeglądarce
     return ( // metoda render zwraca obiekt JSX, a konkretniej div
       <main className={styles.component}>
-        <h1 className={styles.title}>My first React app</h1>
-        <h2 className={styles.subtitle}>A simple to-do app, with lists, columns and cards</h2>
-        <List title={['Things to do', <sup key='1'>soon!</sup>]} imgUrl={"http://uploads.kodilla.com/bootcamp/fer/11.react/space.png"} />
+        <h1 className={styles.title}>{pageContents.title}</h1>
+        <h2 className={styles.subtitle}>{pageContents.subtitle}</h2>
+        {/*<List title={['Things to do', <sup key='1'>soon!</sup>]} imgUrl={"http://uploads.kodilla.com/bootcamp/fer/11.react/space.png"} />*/}
+        <List {...listData} />
       </main>
     )
   }
