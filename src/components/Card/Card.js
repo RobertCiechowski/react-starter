06c9021a4d157a04bdfdb47aadcece607d4d9683
Robert Ciechowski <croberttm@gmail.com>
2020-07-24 14:37:03 +0200

    Fixed errors found by ESLint

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,18 +3,18 @@ import styles from './Card.scss';
 import PropTypes from 'prop-types';
 
 class Card extends React.Component {
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-    }
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+  }
 
-    render () {
-        const {title} = this.props;
-        return (
-            <section className={styles.Component}>
-                <h3>{title}</h3>
-            </section>
-        );
-    }
+  render() {
+    const { title } = this.props;
+    return (
+      <section className={styles.Component}>
+        <h3>{title}</h3>
+      </section>
+    );
+  }
 }
 
 export default Card;
