e16ff553f863bea8dfca9f3fadd09ed978c53c11
Robert Ciechowski <croberttm@gmail.com>
2020-07-30 18:09:43 +0200

    Adding columns done (12.4.1).

--- src/components/List/ListContainer.js
+++ src/components/List/ListContainer.js
@@ -1,5 +1,6 @@
 import { connect } from 'react-redux';
 import List from './List';
+import { getColumnsForList, createActionAddColumn } from '../../../src/redux/columnsRedux.js';
 
 /* stary mapStateToProps
 const mapStateToProps = state => ({ // w stałej mapStateToProps zapisujemy funkcję, która definiuje powiązanie propsów z stanem
@@ -11,7 +12,16 @@ const mapStateToProps = (state, props) => ({ // w stałej mapStateToProps zapisu
   columns: getColumnsForList(state, props.id),
 });
 
+/* 
 export const getColumnsForList = ({ columns }, listId) => columns.filter(column => column.listId == listId);
+*/
 
-export default connect(mapStateToProps)(List); // odpowiedzialne za połączenie komponentu App z magazynem, czyli sprawia, że wszystko działa
+const mapDispatchToProps = (dispatch, props) => ({
+  addColumn: title => dispatch(createActionAddColumn({
+    listId: props.id,
+    title,
+  })),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(List); // odpowiedzialne za połączenie komponentu App z magazynem, czyli sprawia, że wszystko działa
 // w ostatniej parze nawiasów musimy podać komponent, który jest wykorzystywany w tym kontenerze
